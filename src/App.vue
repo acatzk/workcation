0ca80219ad6fb73847674a74702859b32b37831c
Joshua Galit <joshuaimalay@gmail.com>
2020-12-13 17:03:12 +0800

    Main section done with mobile first design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app" class="bg-gray-200 antialiased">
+  <div id="app" class="bg-gray-200 antialiased min-h-screen">
+    <!-- Header section -->
     <header class="flex items-center justify-between bg-gray-900 px-4 py-3">
       <svg class="h-8 w-auto" 
            viewBox="0 0 185 32" 
@@ -31,6 +32,7 @@
         </svg>
       </button>
     </header>
+    <!-- Filter section -->
     <section class="flex justify-between bg-gray-800 px-4 py-3">
       <div class="relative">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -57,12 +59,46 @@
               xmlns="http://www.w3.org/2000/svg">
           <path d="M3 6a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1zm3 6a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1zm4 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4z" />
         </svg>
-        <span class="ml-1 text-white font-medium">Filters</span>
+        <span class="ml-1 text-gray-400 font-medium">Filters</span>
       </button>
     </section>
-    <main>
-      <h3>Los Angeles</h3>
-      <p>Live like the stars in these luxurious Southern Califonia estates.</p>
+    <!-- main section -->
+    <main class="px-4 py-6">
+      <h3 class="text-xl text-gray-900 font-medium">Los Angeles</h3>
+      <p class="text-gray-600">Live like the stars in these luxurious Southern Califonia estates.</p>
+      <div class="mt-6">
+        <div>
+          <div>
+            <img class="rounded-lg shadow-md h-64" 
+               src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" 
+               alt="house-image">
+          </div>
+          <div class="relative px-4 -mt-16">
+            <div class="bg-white px-4 py-4 rounded-lg shadow-lg">
+              <div class="flex">
+                <span class="bg-green-200 text-green-800 font-semibold rounded-full px-2 py-1 leading-none inline-block text-xs uppercase tracking-wide">Plus</span>
+                <div class="ml-2 text-sm text-gray-600 font-semibold tracking-wide uppercase">
+                  3 Beds &bull; 2 baths
+                </div>
+              </div>
+              <h4 class="mt-1 text-lg text-gray-900 font-semibold">Modern home in city center</h4>
+              <div class="mt-1">
+                <span class="text-gray-900">$1,400</span>
+                <span class="ml-1 text-sm text-gray-600">/wk</span>
+              </div>
+              <div class="mt-2 text-sm text-gray-600 flex items-center">
+                <svg v-for="i in 5" :key="i" 
+                     class="h-4 w-4 fill-current text-green-400"
+                     viewBox="0 0 24 24" 
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"/>
+                </svg>
+                <span class="ml-2">34 reviews</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>

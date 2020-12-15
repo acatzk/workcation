54219af2d9c30423b2cf98c0baf27ae70e91bf6e
Joshua Galit <joshuaimalay@gmail.com>
2020-12-15 13:30:16 +0800

    Exteded spacing 80 with 20rem size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      spacing: {
+        '80': '20rem'
+      },
       padding: {
         '5/6': '83.3333333%'
       }
